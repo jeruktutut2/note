7939816b1d2f7452a6aee1386e6786fa3a2fb616
jeruktutut2 <jeruktutut2@gmail.com>
2025-05-01 16:17:59 +0800

    add remote routing and vue dynamic remote

--- microfrontend/README.md
+++ microfrontend/README.md
@@ -13,6 +13,9 @@
     npm install -D tailwindcss @tailwindcss/vite @originjs/vite-plugin-federation
 
     npm install vue-router@4
+    npm i react-router-dom
+    react: { singleton: true, eager: true },
+    'react-dom': { singleton: true, eager: true }
 
     to run remote on host, you have to npm run build and npm run preview remote app, remoteEntry.js is in dist/assets/remoteEntry, use this (http://localhost:3001/assets/remoteEntry.js) on vite config host
 
---
+++ microfrontend/dynamic_remote/vue/host/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+    import { defineAsyncComponent } from 'vue'
+    import { loadRemote } from './loadRemote'
+    
+    const RemoteButton = defineAsyncComponent(() => loadRemote('remote', 'http://localhost:3001', './Button'))
+    const RemoteText = defineAsyncComponent(() => loadRemote('remote', 'http://localhost:3001', './Text'))
+</script>
+
+<template>
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+    <!-- <div>
+        <button class="bg-blue-500 text-white px-4 py-2 rounded">Button Host</button>
+    </div> -->
+    <RemoteButton />
+    <RemoteText />
+    <p>
+        <strong>Current route path:</strong> {{ $route.fullPath }}
+    </p>
+    <nav>
+        <RouterLink to="/">App </RouterLink>
+        <RouterLink to="/about">About </RouterLink>
+        <RouterLink to="/profile">Profile </RouterLink>
+    </nav>
+    <main>
+        <RouterView />
+    </main>
+</template>
+
+<style scoped>
+</style>
